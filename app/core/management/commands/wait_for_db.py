b943e35dab21cd6526f95274d155817e9806ab59
Aleks <aleks@JustComputer.local>
2023-01-25 18:46:37 +0200

    wait for db time update

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,6 +16,6 @@ class Command(BaseCommand):
                 db_up = True
             except (Psycopg20pError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)
+                time.sleep(2)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
