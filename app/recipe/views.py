cf0e0651ac7bb063bc8b8cdecb8b927624857640
Aleks <aleks@JustComputer.local>
2023-02-04 16:58:43 +0200

    openAI removed

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,3 @@
-from drf_spectacular.utils import (
-    extend_schema_view,
-    extend_schema,
-    OpenApiParameter,
-    OpenApiTypes
-)
-
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -15,22 +8,6 @@ from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
-@extend_schema_view(
-    list=extend_schema(
-        parameters=[
-            OpenApiParameter(
-                'tags',
-                OpenApiTypes.STR,
-                description='Comma separated list of tag IDs to filter',
-            ),
-            OpenApiParameter(
-                'ingredients',
-                OpenApiTypes.STR,
-                description='Comma separated list of ingredient IDs to filter',
-            ),
-        ]
-    )
-)
 class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
